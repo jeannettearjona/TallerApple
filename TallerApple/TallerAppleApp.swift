ca81f496957c51c3c6fe46ee1015a696de29a80c
jeannette <a01236226@tec.mx>
2023-12-05 14:22:06 -0600

    Initial Commit

---
+++ TallerApple/TallerAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallerAppleApp.swift
+//  TallerApple
+//
+//  Created by Concepción Hernández Flores on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TallerAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
